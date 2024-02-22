0b73ff265d3ed84f78a3cdf75343dfe9c1c9c67e
Eduarda Cunha <eduardagoncalvesdacunha@gmail.com>
2024-02-22 10:22:52 -0300

    faculdade

--- faculdade/computacao/lista-1/days.c
+++ faculdade/computacao/lista-1/days.c
@@ -1,23 +1,36 @@
 #include <stdio.h>
-#include <limits.h>
 
-int main(){
-    int n; scanf("%d", &n);
-    int arr[n+1], arr1[n];
+void lervet(int n, int arr[]){
     for(int i = 0; i < n; i++){
         int a; scanf("%d", &a);
         arr[i] = a;
     }
+}
 
-    arr1[0] = arr[n-1]; 
+void printvet(int arr[], int n){
+    for(int i = 0; i < n; i++){
+        printf("%d ", arr[i]);
+    }
+    printf("\n");  
+}
+
+int* solve(int arr[], int n){
+    int arr1[n+1];
+    arr1[0] = arr[n-1];
     for(int i = 1; i < n; i++){
         arr1[i] = arr[i-1];
     }
-
     for(int i = 0; i < n; i++){
-        printf("%d ", arr1[i]);
+        arr[i] = arr1[i];
     }
-    printf("\n");
+    return arr;
+}
 
-
+int main(){
+    int n; scanf("%d", &n);
+    int arr[n+1];
+    lervet(n, arr);
+    solve(arr, n);
+    printvet(arr, n);
+    return 0;
 }

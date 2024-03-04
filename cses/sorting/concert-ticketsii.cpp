f6bc2582aa5ff129a971fb9f3024946906981dd1
Eduarda Cunha <eduardagoncalvesdacunha@gmail.com>
2024-03-03 21:32:48 -0300

    sorted sets

---
+++ cses/sorting/concert-ticketsii.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n, m; cin >> n >> m;
+    map<int, int> mp;
+    int arr[n+1];
+    for(int i = 0; i < n; i++) cin >> arr[i];
+    for(int i = 0; i < m; i++){
+        int b; cin >> b;
+        mp[b]++;
+    }
+   
+}

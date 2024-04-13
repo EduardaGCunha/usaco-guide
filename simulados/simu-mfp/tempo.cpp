8e3dfaa1a40a8e97c4ebbe559bfe11c2b5280a6e
Eduarda Cunha <eduardagoncalvesdacunha@gmail.com>
2024-04-13 12:48:20 -0300

    simuladinhos

---
+++ simulados/simu-mfp/tempo.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n, m, x, y; cin >> n >> m >> x >> y;
+    if(x > n && y > m){
+        cout << "O JOGO DUROU " << x - n << " HORA(S) E " << m - y << " MINUTO(S)";
+    }else if(x==n && y == n){
+        
+    }
+}

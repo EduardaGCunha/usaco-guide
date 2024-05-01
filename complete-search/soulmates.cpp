ead36a556a87815033d2349a68eb0a230093f8a1
Eduarda Cunha <eduardagoncalvesdacunha@gmail.com>
2024-05-01 12:50:00 -0300

    teste

--- complete-search/soulmates.cpp
+++ complete-search/soulmates.cpp
@@ -5,9 +5,7 @@ int a, b;
 int dp(int a){
     if(a == b) return 0;
     if(a%2 == 0){
-        return min({dp(a+1)+1, dp(a/2)+1, dp(a*2)+1});
-    }else{
-        return min({dp(a+1)+1, dp(a*2)+1});
+        
     }
 }
 

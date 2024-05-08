7ec5c448c5a01dbaf21b53fb0d4f1fec5a51aa91
Eduarda Cunha <eduardagoncalvesdacunha@gmail.com>
2024-05-08 16:54:16 -0300

    path-cost

--- dp/path-cost.cpp
+++ dp/path-cost.cpp
@@ -1,21 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+#define int long long
+
+signed main(){
+    freopen("king2.in", "r", stdin);
+    freopen("king2.out", "w", stdout);
     const int n = 8;
-    int grid[n][n], dp[n][n];
+    int dp[n][n];
     
     for(int i = 0; i < n; i++){
         for(int j = 0; j < n; j++){
-            cin >> grid[i][j];
+            cin >> dp[i][j];
         }
     }
 
-    // for(int i = n; i >= 0; i--){
-    //     for(int j = 0; j < n - 1; j++){
-    //         dp[i][j] = max({dp[i-1][j], dp[i][j+1], dp[i-1][j-1]}) + grid[i][j];
+    //dp[0][n] = grid[0][n];
+    for(int i = n - 1; i >= 0; i--){
+        for(int j = 0; j < n; j++){
+            if(j == 0 && i == n - 1) continue;
+            int sum = 1e18;
+            if(i < n - 1) sum = min(sum, dp[i+1][j]);
+            if(j > 0) sum = min(sum, dp[i][j-1]);
+            if(i < n - 1 && j > 0) sum = min(sum, dp[i+1][j-1]);
+            dp[i][j] += sum;
+        }
+    }
+
+    // for(int i = 0; i < n; i++){
+    //     for(int j = 0; j < n; j++){
+    //         cout << dp[i][j] << " ";
     //     }
+    //     cout << endl;
     // }
 
-    // cout << dp[n-1][0] << endl;
+    cout << dp[0][7] << endl;
 }

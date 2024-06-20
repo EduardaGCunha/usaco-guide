d098c202493c80a25bba2750f898139eec2b07c9
Eduarda Cunha <eduardagoncalvesdacunha@gmail.com>
2024-06-20 17:10:51 -0300

    graph tranversal2

---
+++ silver/graphs/moocast-gold.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MAXN = 1e3;
+pair<int, int> coord[MAXN];
+
+int main(){
+    int n; cin >> n;
+    for(int i = 0; i < n; i++){
+        int x, y; cin >> x >> y;
+        coord[i].first = x;
+        coord[i].second = y;
+    }
+
+    int l = 0, r = 1e6;
+    while(l < r){
+        int mid = (l+r)/2;
+        
+    } 
+}

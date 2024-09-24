518f46be586862a6307bf559e2f402a2ea5c5c83
Eduarda Cunha <eduardagoncalvesdacunha@gmail.com>
2024-09-24 14:19:21 -0300

    bitwise xorsigma

--- silver/bitwise/t.cpp
+++ silver/bitwise/t.cpp
@@ -2,5 +2,5 @@
 using namespace std;
 
 int main(){
-    cout << (1 >> 0) << endl; 
+    cout << (1 << 1) << endl; 
 }
--- silver/bitwise/xorsigma.cpp
+++ silver/bitwise/xorsigma.cpp
@@ -1,26 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define int long long
+#define ll long long
 
-const int MAXN = 2*(1e5);
-int pfx[MAXN+1], arr[MAXN];
-
-signed main(){
+int main(){
     int n; cin >> n;
-    for(int i = 0; i < n; i++) cin >> arr[i];
+    int arr[n];
+    ll ans = 0;
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+        ans += arr[i];
+    }
 
-
+    ans = -ans;
     for(int i = 0; i < 30; i++){
         int sum = 0;
-        
-        /* contador de quantas vezes que a soma de xor seja maior q 0
-        */
-        vector<int> pref = {1, 0};
+        int pfx[]= {1, 0};
         for(auto a : arr){
-            sum ^= (a >> i) & 1;
+            sum ^= (a>>i)&1; 
 
-            ans += pref[s^1] * 1LL << i;
+            ans += pfx[sum ^1] * 1LL << i;
+            pfx[sum]++;
         }
     }
+
+    cout << ans << endl;
 }
